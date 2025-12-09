432ba8f6eab753fd01091d5569ea372ebf2fbe1f
Maddox Sciuchetti <77411637+MaddoxSciuchetti@users.noreply.github.com>
2025-12-09 14:01:42 +0100

    edits to navbar

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Home.css"
+
+
+export const Home = () => {
+
+
+    
+
+    const curDate = new Date();
+    const date = curDate.getHours()
+
+ 
+
+    let greeting = "";
+    if (date>=1 && date <12) {
+        greeting="Guten Morgne Timo";
+    }else if(date>=12 && date<19){
+        greeting="Guten Nachmittag Timo"
+    }else {
+        greeting="Guten Abend Timo"
+    }
+
+
+    return(
+        <div className="home-container">
+            <div>
+                <h1>{greeting}</h1>
+        
+            </div>
+
+        </div>
+    )
+};
